d2a8ddd30eb6e77c0f2af1191fe7d7fd115e410a
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-08-26 11:18:30 +0100

    Shorten URLs to be more manageable

--- mapper/models.py
+++ mapper/models.py
@@ -9,10 +9,15 @@ def new_key():
     return ''.join(random.choice(letters) for i in range(128))
 
 
+def new_url():
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(64))
+
+
 # Create your models here.
 class Map(models.Model):
     map = models.TextField()
-    slug = models.CharField(max_length=128, default=new_key, unique=True)
+    slug = models.CharField(max_length=128, default=new_url, unique=True, auto_created=True)
 
 
 class APIKey(models.Model):
